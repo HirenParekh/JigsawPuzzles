33e981fc807a272703444374002ee090a4a168c5
Hiren.Parekh <hiren.parekh@outlook.com>
2018-10-25 18:59:29 +0530

    first commit

---
+++ js/jigsaw.js
@@ -0,0 +1,172 @@
+var Jigsaw = function () {
+    var container;
+    var puzzleImg = {};
+    var pieces = [];
+    var gridSize = 3;
+
+    var Piece = function (row, col, size) {
+        this.row = row;
+        this.col = col;
+        this.size = size;
+        this.movePiece = function (row, col) {
+            this.newRow = row;
+            this.newCol = col;
+            $(this.pieceEle).animate({
+                left: this.newRow * size,
+                top: this.newCol * size,
+            }, {
+                duration: 350,
+                easeing: 'easeOutExpo'
+            });
+        }
+        this.createPiece = function (container) {
+            this.pieceEle = document.createElement('div');
+            $(this.pieceEle).css({
+                position: 'absolute',
+                left: row * size,
+                top: col * size,
+                height: size - 4 + 'px',
+                width: size - 4 + 'px',
+                margin: '2px',
+                'background-image': 'url(' + puzzleImg.url + ')',
+                'background-position-x': this.row * size * -1,
+                'background-position-y': this.col * size * -1,
+            });
+            $(container).append(this.pieceEle);
+        }
+        this.removePiece = function () {
+            $(this.pieceEle).remove();
+        }
+    }
+
+    function initPuzzle(opt) {
+        opt = opt || {};
+        gridSize = Math.max(opt.gridSize || 3,3);
+        container = opt.container;
+        loadPuzzleImage(opt.image);
+    }
+
+    function loadPuzzleImage(image) {
+        let img = document.createElement('img');
+        img.onload = function (e) {
+            puzzleImg.height = img.naturalHeight;
+            puzzleImg.width = img.naturalWidth;
+            puzzleImg.url = img.src;
+            var piceSize = puzzleImg.width / gridSize;
+            $(container).css({
+                width: gridSize * piceSize,
+                height: gridSize * piceSize
+            })
+            preparePieces();
+            setTimeout(function () {
+                resetGame();
+            }, 1000)
+        }
+        img.src = image || 'images/micky.png';
+    }
+
+    function resetGame() {
+        shuffle(pieces);
+        var index = 0;
+        for (var i = 0; i < gridSize; i++) {
+            for (var j = 0; j < gridSize; j++) {
+                var piece = pieces[index];
+                if (!piece) {
+                    return;
+                }
+                $(piece.pieceEle).on('click', function () {
+                    var clickedPiece;
+                    for (var i = 0; i < pieces.length; i++) {
+                        if (this === pieces[i].pieceEle) {
+                            clickedPiece = pieces[i];
+                            break;
+                        }
+                    }
+
+                    if (!clickedPiece) {
+                        return;
+                    }
+                    var newRow = clickedPiece.newRow;
+                    var newCol = clickedPiece.newCol - 1;
+                    if (newRow >= 0 && newCol >= 0) {
+                        if (!getPiece(newRow, newCol)) {
+                            clickedPiece.movePiece(newRow, newCol);
+                            return;
+                        }
+                    }
+                    newRow = clickedPiece.newRow - 1;
+                    newCol = clickedPiece.newCol;
+                    if (newRow >= 0 && newCol >= 0) {
+                        if (!getPiece(newRow, newCol)) {
+                            clickedPiece.movePiece(newRow, newCol);
+                            return;
+                        }
+                    }
+                    newRow = clickedPiece.newRow + 1;
+                    newCol = clickedPiece.newCol;
+                    if (newRow < gridSize && newCol < gridSize) {
+                        if (!getPiece(newRow, newCol)) {
+                            clickedPiece.movePiece(newRow, newCol);
+                            return;
+                        }
+                    }
+                    newRow = clickedPiece.newRow;
+                    newCol = clickedPiece.newCol + 1;
+                    if (newRow < gridSize && newCol < gridSize) {
+                        if (!getPiece(newRow, newCol)) {
+                            clickedPiece.movePiece(newRow, newCol);
+                            return;
+                        }
+                    }
+                });
+                piece.movePiece(i, j);
+                index++;
+            }
+        }
+    }
+
+    function getPiece(row, col) {
+        for (var i = 0; i < pieces.length; i++) {
+            var currPiece = pieces[i];
+            if (currPiece.newRow == row && currPiece.newCol == col) {
+                return currPiece;
+            }
+        }
+        return null;
+    }
+
+    function preparePieces() {
+        var piceSize = puzzleImg.width / gridSize;
+        for (var i = 0; i < gridSize; i++) {
+            for (var j = 0; j < gridSize; j++) {
+                var piece = new Piece(i, j, piceSize);
+                piece.createPiece(container);
+                pieces.push(piece);
+            }
+        }
+        pieces.pop().removePiece();
+    }
+
+    function shuffle(array) {
+        var m = array.length,
+            t, i;
+
+        // While there remain elements to shuffle…
+        while (m) {
+
+            // Pick a remaining element…
+            i = Math.floor(Math.random() * m--);
+
+            // And swap it with the current element.
+            t = array[m];
+            array[m] = array[i];
+            array[i] = t;
+        }
+
+        return array;
+    }
+
+    return {
+        init: initPuzzle
+    }
+};
---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    colors = ['#795548', '#f44336', '#03a9f4'];
+    new Jigsaw().init({
+        container: '#slide0',
+        image: 'images/jerry.png',
+        gridSize: 3
+    });
+    new Jigsaw().init({
+        container: '#slide1',
+        image: 'images/micky.png',
+        gridSize: 3
+    });
+    new Jigsaw().init({
+        container: '#slide2',
+        image: 'images/superman.png',
+        gridSize: 3
+    });
+
+    var slider = $(".slider").slick({
+        slidesToShow: 1,
+        variableWidth: true,
+        adaptiveHeight: true,
+        infinite: false,
+        prevArrow: '<div class="slick-prev" aria-label="Previous" ><div></div></div>',
+        nextArrow: '<div class="slick-next" aria-label="Next"><div></div></div>',
+    });
+    $('.slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        let slide = '#slide' + nextSlide;
+        console.log(slide);
+        var newWidth = $(slide).width();
+        console.log(newWidth);
+        $('.wrapper').animate({
+            width: newWidth
+        }, 400);
+        $('body').css({
+            'background-color': colors[nextSlide]
+        }, 300);
+        setTimeout(function () {
+            $('.slider').slick('resize');
+        }, 500)
+    });
+})
